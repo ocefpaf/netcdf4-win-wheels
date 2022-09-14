a932dd9c338ffc0f89cbceae78a78d4bdb2c77e7
Filipe Fernandes <ocefpaf@gmail.com>
2022-09-14 16:40:55 -0300

    should fail when tests are failing

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,5 +3,5 @@ for %%f in (%PREFIX%\*.whl) do (
 )
 "%PYTHON%" -m pip install pytest cython
 
-cd test && "%PYTHON%" run_all.py
-"%PYTHON%" -c "import netCDF4"
+cd test && "%PYTHON%" run_all.py || exit 1
+"%PYTHON%" -c "import netCDF4" || exit 1
