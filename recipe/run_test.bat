7f1e3f6a7b0a9944ccd39aa07973ebcdb6ee7fbb
Filipe Fernandes <ocefpaf@gmail.com>
2022-09-14 16:14:47 -0300

    missing cython

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,7 +1,7 @@
 for %%f in (%PREFIX%\*.whl) do (
   "%PYTHON%" -m pip install %%f
 )
-"%PYTHON%" -m pip install pytest
+"%PYTHON%" -m pip install pytest cython
 
 cd test && "%PYTHON%" run_all.py
 "%PYTHON%" -c "import netCDF4"
